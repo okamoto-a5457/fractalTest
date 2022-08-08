c5d78bb7a60c7161c76543a88e0a6fdd02cf15bb
okamoto-a5457 <104484908+okamoto-a5457@users.noreply.github.com>
2022-08-08 13:07:34 +0900

    made test compornents.

---
+++ styleGuide/dist/components/raw/btn-secondary/index.js
@@ -0,0 +1,5 @@
+$(function () {
+  $(".button01").on("click", function () {
+    alert("clicked！");
+  });
+});
--- styleGuide/fractal.config.js
+++ styleGuide/fractal.config.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 
 /*
-* Require the path module
-*/
-const path = require('path');
+ * Require the path module
+ */
+const path = require("path");
 
 /*
  * Require the Fractal module
  */
-const fractal = module.exports = require('@frctl/fractal').create();
+const fractal = (module.exports = require("@frctl/fractal").create());
 
 /*
  * Give your project a title.
  */
-fractal.set('project.title', 'Style Guide');
+fractal.set("project.title", "Style Guide");
 
 /*
  * Tell Fractal where to look for components.
  */
-fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set("path", path.join(__dirname, "components"));
 
 /*
  * Tell Fractal where to look for documentation pages.
  */
-fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set("path", path.join(__dirname, "docs"));
 
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', path.join(__dirname, 'public'));
+fractal.web.set("static.path", path.join(__dirname, "public"));
+
+/* 静的ファイルの書き出し場所 */
+fractal.web.set("builder.dest", __dirname + "/dist");
